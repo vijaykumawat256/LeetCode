53817a3a9c349bda156726e07e908bfae25eb5cd
vijaykumawat256 <37927929+vijaykumawat256@users.noreply.github.com>
2023-01-16 22:48:31 -0700

    Time: 11 ms (53.17%), Space: 46.3 MB (96.16%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    boolean overlap(int[] a, int[] b){
+        return Math.min(a[1],b[1]) - Math.max(a[0],b[0]) >=0;
+    }
+    
+    int[] mergerInterval(int[] a, int[] b){
+        int[] newInterval = {Math.min(a[0],b[0]),Math.max(a[1],b[1])};
+        return newInterval;
+        
+    }
+    
+    public int[][] merge(int[][] intervals) {
+        
+        System.out.println(Arrays.toString(intervals[0]));
+        
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        System.out.println(Arrays.toString(intervals[0]));
+        
+        List<int[]> newArr = new ArrayList<>();
+        for(int i=0;i<intervals.length;i++){
+            int[] currInterval = {intervals[i][0],intervals[i][1]};
+            while(i<intervals.length && overlap(currInterval,intervals[i])){
+                currInterval = mergerInterval(currInterval,intervals[i]);
+                i++;
+            }
+            i--;
+            newArr.add(currInterval);
+        }
+        return newArr.toArray(new int[newArr.size()][2]);
+        
+        
+    }
+}
