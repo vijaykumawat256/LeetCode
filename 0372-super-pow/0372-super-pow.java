a7aa843437dbb5dfa24ed581071ef76691fd1b29
vijaykumawat256 <37927929+vijaykumawat256@users.noreply.github.com>
2023-03-05 00:13:57 -0700

    Time: 1 ms (100.00%), Space: 42.6 MB (33.76%) - LeetHub

--- 0372-super-pow/0372-super-pow.java
+++ 0372-super-pow/0372-super-pow.java
@@ -1,7 +1,7 @@
 class Solution {
-    public int superPow(long a, int[] b) {
-        int ans = 0;
-        int x = 1;
+    public int superPow(int a, int[] b) {
+        int ans=0;
+     int x = 1;
         // For Making b a digit
         for(int i = 0; i < b.length ; i++){
              ans = (ans*10 + b[i])%1140;
@@ -10,14 +10,16 @@ class Solution {
               }
                 x = x * 10;
         }
-       // finding power of a
+        
         a = a % 1337;
         long ans1 = 1;
         for(int i = 0; i < ans;i++){
             ans1=(ans1*a)%1337;
-            System.out.println(ans1);
         }
-        int ans2 = (int)ans1;
-        return ans2;
+        return (int)ans1;
+      
+        
+        
+        
     }
 }
