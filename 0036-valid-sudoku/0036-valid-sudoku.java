3b542f9074313cc592ed7dc19720e5df6ef690b6
vijaykumawat256 <37927929+vijaykumawat256@users.noreply.github.com>
2023-03-07 00:26:36 -0700

    Time: 3 ms (67.23%), Space: 43.2 MB (25.89%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        int n = 9;
         
-        int N = 9;
-        HashSet<Character>[] rows = new HashSet[N];
-        HashSet<Character>[] cols = new HashSet[N];
-        HashSet<Character>[] boxes = new HashSet[N];
+        HashSet<Character>[] rows = new HashSet[n];
+        HashSet<Character>[] cols = new HashSet[n];
+        HashSet<Character>[] boxes = new HashSet[n];
         
-        for(int r =0;r<N; r++){
-            rows[r]= new HashSet<Character>();
-            cols[r]=new HashSet<Character>();
-            boxes[r]=new HashSet<Character>();
+        
+        for(int r=0;r<n;r++){
+            rows[r] = new HashSet<>();
+            cols[r] = new HashSet<>();
+            boxes[r]= new HashSet<>();
         }
         
-        for(int r=0; r<N;r++){
-            for(int c=0;c<N;c++){
+        for(int r = 0;r<n;r++){
+            for(int c=0;c<n;c++){
                 
                 char val = board[r][c];
                 
@@ -31,7 +32,7 @@ class Solution {
                 }
                 cols[c].add(val);
                 
-                int idx = (r/3)*3 +c/3;
+                int idx = (r/3)*3 + c/3;
                 
                 if(boxes[idx].contains(val)){
                     return false;
@@ -40,12 +41,5 @@ class Solution {
             }
         }
         return true;
-        
-            
-        
-        
-        
-        
-        
     }
 }
