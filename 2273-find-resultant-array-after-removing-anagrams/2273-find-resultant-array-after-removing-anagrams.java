b9a0222e4b85b72371a81859dcdd0c20241f85a3
vijaykumawat256 <37927929+vijaykumawat256@users.noreply.github.com>
2023-03-19 17:00:30 -0700

    Time: 3 ms (74.11%), Space: 42.5 MB (76.41%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -1,29 +1,21 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
        
-        String prev ="";
-        List<String> li=new ArrayList<>();
+     
+        
+        String prev = "";
+        List<String> li = new ArrayList<>();
         for(int i=0;i<words.length;i++){
-            
             String s1 = words[i];
-       
-            
-            char[] arr1 = s1.toCharArray();
-               
-            Arrays.sort(arr1);
-            
-            String str1 =  String.valueOf(arr1);
-            if(!str1.equals(prev)){
+            char[] arr = s1.toCharArray();
+            Arrays.sort(arr);
+            String str = new String(arr);
+            if(!str.equals(prev)){
                 li.add(words[i]);
-                prev = str1;
-                
+                prev = str;
             }
-            
-            
         }
         return li;
-
-        
         
     }
 }
